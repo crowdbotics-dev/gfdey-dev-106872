6990c4f44a20eaac9a38f07276a978c4d6d532ba
ishubham1995 <shubham.malik@crowdbotics.com>
2023-08-03 22:17:11 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "gfdey-dev-106872",
-  url: "https://gfdey-dev-106872.botics.co",
-  api: "https://gfdey-dev-106872.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "gfdey-dev-106872",
+  "url": "https://gfdey-dev-106872.botics.co",
+  "api": "https://gfdey-dev-106872.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": ""
 };
 export const modulesOptions = {
   "@modules/app-menu": {
